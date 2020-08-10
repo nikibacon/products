5a6f2ed500b3f80ff24e589e1a74dc124ac4e95f
NIKI <niki801226@hotmail.com>
2020-08-10 15:18:27 +0800

    isfile

--- products.py
+++ products.py
@@ -1,15 +1,24 @@
-products = []
+import os # operating system
 
-#讀取檔案
-with open('products.csv', 'r',encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue		
-		name, price = line.strip().split(',')
-		#s = line.strip().split(',')
-		# name = s[0]
-		# price = s[1]
-		products.append([naem, price])
+products = [] #不管檔案存不存在都需要清單
+
+if os.path.isfile('products.csv'):
+  #相對路徑,同一個資料夾
+  #os的小模組path裡面的功能isfile可以去檢查檔案在不在
+	print('got it !')
+  #讀取檔案
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue		
+			name, price = line.strip().split(',')
+			#s = line.strip().split(',')
+			# name = s[0]
+			# price = s[1]
+			products.append([name, price])
+else:
+	print('Oops... nothing here')
+	
 
 #讓使用者輸入
 while True:
