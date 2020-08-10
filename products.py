ad94e98f3d8464545e02fe5a21ec0931da4ad813
NIKI <niki801226@hotmail.com>
2020-08-10 10:36:14 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,22 @@
+
+products = []
+
+while True:
+	name = input('清輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	#建立小清單
+	#p = []
+	#p.append(name)
+	#p.append(price)
+
+	#P = [name, price]
+
+	#放進大清單
+	#products.append(p)	
+	products.append([name, price])	
+
+print(products)
+
+
